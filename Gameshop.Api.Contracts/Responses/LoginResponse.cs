043481a385d7e93d5c7f68e967809da8e2b54e96
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2016-10-26 22:28:15 +0800

    Added API contracts to separate API requests and responses from the other contracts. This contract is to be used by website and mobile app.

---
+++ Gameshop.Api.Contracts/Responses/LoginResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gameshop.Api.Contracts.Responses
+{
+    public class LoginResponse : ApiResponse
+    {
+        public string Token { get; set; }
+    }
+}
---
+++ Gameshop.Api.Contracts/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gameshop.Api.Contracts
+{
+    public enum Result
+    {
+        Unknown,
+        Success,
+        Error
+    }
+}
