bf40adf5f9d343dc01340e70111a2a6c83fb8c0f
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2016-10-26 22:43:35 +0800

    Create a separate API contract apart from the main contract. This will contain API request and resposne objects.

---
+++ src/Gameshop.Api.Contracts/Responses/ApiResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gameshop.Api.Contracts.Responses
+{
+    public class ApiResponse
+    {
+        public Result Result { get; set; }
+    }
+}
