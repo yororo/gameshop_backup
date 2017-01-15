16f30c7d19998f0a1d48a7740d0943fc9d7d06d4
jeyjeyemem <joeljeremy.marquez@gmail.com>
2017-01-16 01:24:55 +0800

    Update GameShop.Api.Contracts AssemblyInfo

--- src/Gameshop.Api.Contracts/Properties/AssemblyInfo.cs
+++ src/Gameshop.Api.Contracts/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Gameshop.Api.Contracts")]
+[assembly: AssemblyProduct("GameShop.Api.Contracts")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
